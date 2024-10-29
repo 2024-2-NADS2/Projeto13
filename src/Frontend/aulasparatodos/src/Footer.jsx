d1b77f1c622aad5b6ccd6cd1cdfeb46c23d9e7fc
rafael budoya <budoyya@gmail.com>
2024-10-29 12:36:21 -0300

    correção

--- src/Frontend/aulasparatodos/src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(){
-    return(
-        <footer>
-        <p>&copy; Projetop 2024</p>
-      </footer>
-    )
-}
--- src/Frontend/aulasparatodos/src/Titulo.jsx
+++
@@ -1,9 +0,0 @@
-export default function Titulo(){
-    return(
-        <section className="hero">
-          <h1>Aprenda online com as melhores video aula</h1>
-          <p>Descubra vídeos didáticos e testes assim como formas de aplicar seu conhecimento, tudo isso ainda hoje ao alcance de sua casa e no seu tempo.</p>
-          <a href="#" className="btn">Entrar!!!</a>
-        </section>
-    )
-}
